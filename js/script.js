f2ceddae1637153b914f2d3bfb3528abeaafd460
simolorevg <simoneviaggi@libero.it>
2023-03-17 17:36:23 +0100

    all the afternoon to do this, sorry

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const numberList = [];
+for(let i = 1; i <= 100; i++){
+    numberList.push(i);
+}//creo array di numeri
+for(let x = 0; x < numberList.length; x++){
+    document.getElementById('number-grid').innerHTML += `<div class = 'grid-item'>${numberList[x]}</div>`;
+}
