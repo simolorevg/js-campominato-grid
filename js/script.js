bc97211c43a4f0739a67404357990e17f0374fc1
simolorevg <simoneviaggi@libero.it>
2023-03-17 17:47:22 +0100

    This is all i have done toady

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ for(let i = 1; i <= 100; i++){
 for(let x = 0; x < numberList.length; x++){
     document.getElementById('number-grid').innerHTML += `<div class = 'grid-item'>${numberList[x]}</div>`;
 }
+////////////////
+//creo la funzione
+function whatIsThisNumber(){
+    this.classList.toggle('green');
+}
+
