8ce9cdef0f5f9510c4115a8d3577e7fb40d777aa
simolorevg <simoneviaggi@libero.it>
2023-03-18 10:05:25 +0100

    Working on the script

--- js/script.js
+++ js/script.js
@@ -4,14 +4,33 @@ const numberList = [];
 for(let i = 1; i <= 100; i++){
     numberList.push(i);
 }
-//assegno gli elementi dell'array al div number-grid dinamicamente
 const numberGrid = document.getElementById('number-grid');
 console.log(numberGrid);
-
-////////////////
 //creo la funzione
+/**
+ * 
+ * @returns {StyleSheet}
+ */
 function greenNumber(){
     return this.classList.toggle('green');
 }
+/**
+ * Descrizione: quando viene avviato, mostra a schermo il suo contenuto
+ * @param {string}
+ * @returns {String}
+ */
+function alertNumber(a){
+    return alert(a);
+}
+/**
+ * Descrizione: serve per poter inserire automaticamente i numeri all'interno della griglia
+ * @param {HTMLElement} a
+ * @returns {HTMLElement}
+ */
+function createGridNumber(a){
+    
+}
+//inserisco dinamicamente i numeri all'interno dell'array
+
 
 
