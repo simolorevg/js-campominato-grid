3a45e1e4650987450d92a34e733ff9d454ad742b
simolorevg <simoneviaggi@libero.it>
2023-03-18 10:32:05 +0100

    Script done

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@ const numberList = [];
 for(let i = 1; i <= 100; i++){
     numberList.push(i);
 }
-const numberGrid = document.getElementById('number-grid');
-console.log(numberGrid);
-//creo la funzione
+//creo le funzioni
 /**
  * 
  * @returns {StyleSheet}
@@ -19,8 +17,9 @@ function greenNumber(){
  * @param {string}
  * @returns {String}
  */
-function alertNumber(a){
-    return alert(a);
+function alertNumber(){
+    let numberContent = this.innerText;
+    return alert(numberContent);
 }
 /**
  * Descrizione: serve per poter inserire automaticamente i numeri all'interno della griglia
@@ -28,9 +27,20 @@ function alertNumber(a){
  * @returns {HTMLElement}
  */
 function createGridNumber(a){
-    
+    const gridItemNumber = document.createElement('div');//creo un elemento html div
+    gridItemNumber.innerText = a;//ci inserisco un numero
+    gridItemNumber.classList.add('grid-item');//aggiungo al div la classe grid-item
+    return gridItemNumber;//consegno come risultato l'elemento terminato
 }
-//inserisco dinamicamente i numeri all'interno dell'array
-
+const numberGrid = document.getElementById('number-grid');//creo l'elemento che conterrà i numero nella griglia
+console.log(numberGrid);
+let gridNumber;//questa variabile mi servirà sia per l'inserimento dinamico dei numeri, ma anche per la visione del numero al click più tardi
+//inizio l'inserimento dinamico degli elementi
+for(let x = 0; x < numberList.length; x++){
+    gridNumber = createGridNumber(x);//eseguo la funzione createGridNumber per ogni elemento dell'array
+    gridNumber.addEventListener('click', greenNumber);//al click faccio diventare lo sfondo della cella verde
+    gridNumber.addEventListener('click', alertNumber);//al click mostra il numero
+    numberGrid.append(gridNumber);// lo inserisco all'interno della griglia genitore
+}
 
 
